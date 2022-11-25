e073fd9127fdb9a6e5b46a89916b6d4be4a9d5d1
chnyang <chn.yang@qq.com>
2022-11-25 23:11:36 +0800

    优化

--- src/com/Main.java
+++ src/com/Main.java
@@ -265,7 +265,7 @@ public class Main extends JFrame implements ActionListener {
         }
     }
 
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         new Main();
     }
 
